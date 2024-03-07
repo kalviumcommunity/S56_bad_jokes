bc70f03d8bb3b33c5c731ce7e401f5b4640d89cd
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-03-07 12:51:40 +0530

    resolved a error

--- client/Bad_Jokes/src/Pages/Home.jsx
+++ client/Bad_Jokes/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
     const [jokes, setJokes] = useState([]);
