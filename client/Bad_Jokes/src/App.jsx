0daddcf6475a92833e1e5ac8bde3e61dcd254916
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-03-11 10:26:23 +0530

    added login page with cookie

--- client/Bad_Jokes/src/App.jsx
+++ client/Bad_Jokes/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Form from './Pages/Form';
 import Update from './Pages/Update';
+import Login from './Pages/Login';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/form" element={<Form />} />
         <Route path="/update/:id" element={<Update />} />
       </Routes>
